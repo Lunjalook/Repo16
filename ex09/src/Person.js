a91be2c5f8c691b71113c0ee792a229237ea268a
Lunjalook <azra.cosickic@gmail.com>
2022-04-13 07:58:27 +0200

    Adding exercise 09 - Import a default export

--- ex09/src/Person.js
+++ ex09/src/Person.js
@@ -1,12 +1,12 @@
 class Person {
     constructor(name, email, age) {
-        this.name = name;
-        this.email = email;
-        this.age = age;
+        this._name = name;
+        this._email = email;
+        this._age = age;
     }
 
     getPerson() {
-        let personInfo = `Name: ${this.name}, email: ${this.email}, age: ${this.age}`;
+        let personInfo = `Name: ${this._name}, email: ${this._email}, age: ${this._age}`;
         return personInfo;
     }
 
